f5a7ad6f9b4cd8090dd4fee81a65f22ba9649d3e
Nick Kershner <nskershner@gmail.com>
2025-12-11 04:48:10 +0000

    File changes

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Label } from "@/components/ui/label";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter, DialogDescription } from "@/components/ui/dialog";
 import { toast } from "sonner";
 import {
   DropdownMenu,
