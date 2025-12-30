ff60dbefb51fa8b5355636a06e879db9c5150373
Nick Kershner <nskershner@gmail.com>
2025-12-30 04:54:19 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -64,6 +64,11 @@ export default function Layout({ children, currentPageName }) {
         icon: Copy,
       },
       {
+        title: "Merge Data",
+        url: createPageUrl("MergeData"),
+        icon: Package,
+      },
+      {
         title: "Add Product",
         url: createPageUrl("AddProduct"),
         icon: Upload,
